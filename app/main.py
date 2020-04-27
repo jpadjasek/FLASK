b928ce0dbcf29783281030413fe8be1c3e60439b
Jaroslaw Padjasek <jpadjasek@pgs-soft.com>
2020-04-27 11:49:39 +0200

    Basic rest operations

--- app/main.py
+++ app/main.py
@@ -1,42 +1,103 @@
 from dataclasses import dataclass
 
 import sqlalchemy
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, Response
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+
+
+@dataclass
+class Skill(db.Model):
+    id: int
+    name: str
+    user_id: int
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False, unique=False)
 
 
 @dataclass
 class User(db.Model):
     id: int
-    username: str
-    email: str
+    first_name: str
+    last_name: str
+    cv_url: str
+    skills: Skill
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    first_name = db.Column(db.String(20), unique=True, nullable=False)
+    last_name = db.Column(db.String(120), unique=False, nullable=False)
+    cv_url = db.Column(db.String(120), unique=True, nullable=True)
+    skills = db.relationship('Skill', backref='user', cascade='all', lazy=True)
 
 
-@app.route("/users")
-def hello():
+@app.route('/user', methods=['GET'])
+def get_users():
     users = User.query.all()
-    print(users)
     return jsonify(users)
 
 
-@app.route("/")
-def add():
-    user_1 = User(username='Wacek', email='wacek@gmail.com')
-    db.session.add(user_1)
+@app.route('/user', methods=['POST'])
+def add_user():
+    content = request.get_json()
+    user = User(first_name=content['first_name'], last_name=content['last_name'])
+    db.session.add(user)
     try:
         db.session.commit()
-        return jsonify(user_1)
-    except sqlalchemy.exc.IntegrityError:
-        return "Already exists"
+        return jsonify(user)
+    except sqlalchemy.exc.IntegrityError as e:
+        print(e)
+        return get_error_response(400, message='User already exists')
+
+
+@app.route('/user/<user_id>/skill', methods=['POST'])
+def delete_user_by_id(user_id):
+    try:
+        user = get_user_by_id(user_id)
+
+    except UserNotFoundException:
+        return get_error_response(400, message='User not found')
+
+    content = request.get_json()
+    skill = Skill(name=content['name'], user_id=user.id)
+    db.session.add(skill)
+
+    db.session.commit()
+    return jsonify(skill)
+
+
+@app.route('/user/<user_id>', methods=['DELETE'])
+def add_skill_to_user(user_id):
+    try:
+        user = get_user_by_id(user_id)
+
+    except UserNotFoundException:
+        return Response(status=204, mimetype='application/json')
+    db.session.delete(user)
+    db.session.commit()
+
+    return Response(status=204, mimetype='application/json')
+
+
+def get_user_by_id(user_id):
+    user = User.query.get(user_id)
+    if not user:
+        raise UserNotFoundException
+    return user
+
+
+def get_error_response(status_code, message):
+    body = f"{{nmessage: {message}}}"
+    return Response(body, status=status_code, mimetype='application/json')
+
+
+class UserNotFoundException(Exception):
+    pass
 
 
 if __name__ == '__main__':
